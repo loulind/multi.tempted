811075f02df377ba97ae4c217e53776b62d7ee78
Lou Lindsley <4loubird@gmail.com>
2026-05-22 13:21:02 -0400

    Skeleton steps 1 to 2

--- R/multi_tempted.R
+++ R/multi_tempted.R
@@ -3,48 +3,52 @@
 #' Main function of multiTEMPTED
 #'
 #' @param datlists A length M named list of length n lists of matrices.
+#' Each named list element represents a modality.
 #' Each matrix represents a subject. Columns represent sample number.
 #' The first rows represent sampling time points.
 #' Row 2 through row (number features in modality m) + 1 represent feature values
 #'
-#' @param r Number components to decompose M 3d tensors (ie, rank of CP-type decomposition)
-#' Default is r=3.
+#' @param r Number components to decompose the M 3d tensors
+#' (ie, rank of CP-type decomposition). Default is r=3.
 #'
 #' @returns
 #' @export
 #'
 #' @examples
 #'
-multi_tempted_all <- function(datlists, r=3) {
+multi_tempted <- function(datlists, r=3) {
   if (!(length(unique(lengths(datlists))) == 1)) {
     stop("All lists of matrices must be same length")
   }
 
-  # storing number subjects, modalities, and features
   M <- length(datlists)  # number modalities
   n <- length(datlists[[1]])  # number subjects
   for (m in 1:M) {
     p <- sapply(datlists[[m]], nrow)  # number features per modality
     if (!(length(unique(p)) == 1)) {
-      stop(paste("Modality", m, "has inconsistent feature counts across subjects"))
+      stop(paste("Modality", datlists[[m]], "has inconsistent feature counts across subjects"))
     }
+  }
+
+  # STEP 1: Initialize subject and feature loadings per modality
+  ## (a) Subject loadings init'd with equal contribution
+  a <- rep(1/sqrt(n), times = n)
+
+  ## (b) Feature loadings init'd as matrix of leftmost singular vectors of SVD
+  b <- vector("list", length = M)
+  for (m in 1:M) {
+    b[m] <- init_b(datlists[[m]], p[[m]])
   }
 
   # Calculate each component and remove contribution from feature values
   for (l in 1:r) {
-    # STEP 1: Initialize subject and feature loadings per modality
-    ## (a) Subject loadings init'd with equal contribution
-    a <- init_a(n)
-
-    ## (b) Feature loadings init'd as matrix of leftmost singular vectors of SVD
-    b <- vector("list", length = M)
-    for (m in 1:M) {
-      b[m] <- init_b(datlists[[m]], p)
-    }
-
     # STEP 2: Sequentially estimate loadings
     ## (a) Time loadings
+
+
     ## (b) Subject loadings
+
+
     ## (c) Feature loadings
 
     # STEP 3: Remove contribution of current component & repeat steps 1-2 for all components
@@ -53,10 +57,21 @@ multi_tempted_all <- function(datlists, r=3) {
   # STEP 4: Estimate modality-specific scales
 }
 
-init_a <- function(n) {
+init_b <- function(datlist, p) {
+  # 1. Matrication of datalist
+  # 2. Perform SVD
+  # 3. Extract leftmost singular vector
+  return(leftmost_sv)
+}
+
+update_zeta <- function() {  # updates modality-specific time loadings
+  # Kernel ridge regression code
+}
+
+update_a <- function() {  # updates cross-modality shared subject loading
 
 }
 
-init_b <- function(datlists, p) {
+update_b <- function() {  # updates feature loadings
 
 }
