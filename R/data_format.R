48d56438bc02cd95dc8763ab5a1bea81056a1236
Lou Lindsley <4loubird@gmail.com>
2026-05-23 11:06:09 -0400

    Skeleton data formatting

---
+++ R/data_format.R
@@ -0,0 +1,17 @@
+#' Formatting for multiTEMPTED
+#'
+#' @param featuretables
+#' @param timepoints
+#' @param subjID
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+format_multitempted <- function(featuretables, timepoints, subjID) {
+  # 1. Checks validitiy of data
+  # 2. Adjustment based on requested transformation
+  # 3. Construct named list of lists of matrices (names = modality)
+
+  return(datlists)
+}
