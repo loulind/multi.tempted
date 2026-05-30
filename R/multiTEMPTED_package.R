f1f60205dc9359c913a8ec5bfac8e93de9e68810
Lou Lindsley <4loubird@gmail.com>
2026-05-30 11:00:04 -0400

    imports packages

---
+++ R/multiTEMPTED_package.R
@@ -0,0 +1,6 @@
+#' @importFrom stats  quantile lm qnorm reshape
+#' @importFrom np  npreg
+#' @importFrom ggplot2  ggplot geom_line geom_ribbon facet_wrap aes ylab
+#' @importFrom methods  is
+NULL
+#> NULL
